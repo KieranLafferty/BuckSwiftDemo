cfe814c4d9ca362b2365150db200ad10a1f7f36a
Laff <kieran.lafferty@gmail.com>
2017-10-04 15:38:30 -0700

    added back app

---
+++ Apps/BuckSwiftDemo/BuckSwiftDemo/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  BuckSwiftDemo
+//
+//  Created by Kieran Lafferty on 10/4/17.
+//  Copyright © 2017 Kieran Lafferty. All rights reserved.
+//
+
+import UIKit
+
+@objc class ViewController: UIViewController {
+
+}
+
---
+++ Apps/BuckSwiftDemo/BuckSwiftDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Wishbone
+//
+//  Created by Sanju Naik on 4/8/14.
+//  Copyright (c) 2014 Sanju Naik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <BuckSwiftDemoLibrary/BuckSwiftDemoLibrary-swift.h>
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
+
